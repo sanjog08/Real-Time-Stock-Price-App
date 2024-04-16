c168fca84b781b2faa67a70312bf918a4c35e0e1
sanjog <sanjogsinghbhatia2003@gmail.com>
2024-04-17 01:01:49 +0530

    backend of the app

---
+++ src/app.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const request = require('request')
+const cheerio = require('cheerio')
+
+const app = express();
+const port = process.env.PORT || 7000;
+
+app.get("/get_rate/:stock_name", async (req, res) => {
+    const name = req.params.stock_name;
+    console.log(name);
+    const url = `https://www.google.com/finance/quote/${name}:NSE?sa=X&sqi=2&ved=2ahUKEwjuz-ix38aFAxUvRmwGHTMwC0UQ3ecFegQINRAf`;
+    
+    // make a function to request
+    await request(url, price)
+    
+    async function price (err, response, html_rate) {
+        if (!err && response.statusCode == 200) {
+            // rate is the extracted price of stock with symbols and commas
+            // where cost is the filtered price of stock after removing all symbols and commas
+            const rate = await extract_rate(html_rate);
+            const cost = rate.replace(/[^\d.]/g, '');
+            res.status(200).send(cost);
+        }
+        else {
+            console.log(err);
+            res.status(500).send('Error feching stock price, may be wrong company_symbol had been passed');
+        }
+    }
+
+});
+
+async function extract_rate (html_rate) {
+
+    const $ = cheerio.load(html_rate);
+    const element = $('.YMlKec.fxKbKc');
+    const rate = element.text();
+    return rate;
+}
+
+app.listen(port, () => {
+    console.log(`Server is listening at port number ---> ${port} <---`);
+});
